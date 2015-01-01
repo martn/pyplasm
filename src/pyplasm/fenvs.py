d5daf7ea25b1bfe878cd10796fe745d8f197e9f1
Pavel Solin <solin.pavel@gmail.com>
2015-01-01 13:28:54 -0800

    QUAD

--- src/pyplasm/fenvs.py
+++ src/pyplasm/fenvs.py
@@ -4104,13 +4104,42 @@ TRIANGOLO3D = TRIANGLE3D
 # French:
 # Same as in English
 
+# ===================================================
+# QUAD
+# ===================================================
+
+def quadrilateral(*args):
+    raise ExceptionWT("Command quadrilateral() is undefined. Try QUADRILATERAL() instead?")
+def quad(*args):
+    raise ExceptionWT("Command quad() is undefined. Try QUAD() instead?")
+def QUAD(a, b, c, d):
+    if not isinstance(a, list): 
+        raise ExceptionWT("First argument a in QUAD(a, b, c, d) must either be a 2D point [x, y] or a 3D point [x, y, z].")
+    if not isinstance(b, list): 
+        raise ExceptionWT("Second argument b in QUAD(a, b, c, d) must either be a 2D point [x, y] or a 3D point [x, y, z].")
+    if not isinstance(c, list): 
+        raise ExceptionWT("Third argument c in QUAD(a, b, c, d) must either be a 2D point [x, y] or a 3D point [x, y, z].")
+    if not isinstance(d, list): 
+        raise ExceptionWT("Fourth argument d in QUAD(a, b, c, d) must either be a 2D point [x, y] or a 3D point [x, y, z].")
+    la = len(a)
+    lb = len(b)
+    lc = len(c)
+    ld = len(d)
+    m1 = min(la, lb, lc, ld)
+    m2 = max(la, lb, lc, ld)
+    if m1 != m2:
+        raise ExceptionWT("All points a, b, c, d in QUAD(a, b, c, d) must be 2D points, or all must be 3D points.")
+    if m1 != 2 and m1 != 3:
+        raise ExceptionWT("All points a, b, c, d in QUAD(a, b, c, d) must be 2D points, or all must be 3D points.")
+    return BASEOBJ(PLASM_CONVEXHULL([a, b, c, d]))
+QUADRILATERAL = QUAD 
 
 # ===================================================
 # POLYPOINT 
 # ===================================================
 
 def POLYPOINT (points):
-    return Plasm.mkpol( len(points[0]),CAT(points),[ [i] for i in range(len(points))] )
+    return BASEOBJ(Plasm.mkpol( len(points[0]),CAT(points),[ [i] for i in range(len(points))] ))
 
 # ===================================================
 # POLYLINE 
