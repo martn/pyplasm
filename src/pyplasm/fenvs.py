ecbd170e0678d545832a320a67d8cb049e95e1fb
Pavel Solin <solin.pavel@gmail.com>
2015-01-02 22:33:09 -0800

    Bugfix

--- src/pyplasm/fenvs.py
+++ src/pyplasm/fenvs.py
@@ -6955,26 +6955,46 @@ EXTREMES = EXTREMA
 
 ##### TANGRAMS #####
 
-TANGRAM1 = TRIANGLE([2, 2], [4, 4], [0, 4])
-TANGRAM1 = PRISM(TANGRAM1, 0.01)
-COLOR(TANGRAM1, GREEN)
-TANGRAM2 = TRIANGLE([0, 0], [2, 2], [0, 4])
-TANGRAM2 = PRISM(TANGRAM2, 0.01)
-COLOR(TANGRAM2, YELLOW)
-TANGRAM3 = TRIANGLE([3, 3], [4, 2], [4, 4])
-TANGRAM3 = PRISM(TANGRAM3, 0.01)
-COLOR(TANGRAM3, BLUE)
-TANGRAM4 = SQUARE(1.4142)
-TANGRAM4 = PRISM(TANGRAM4, 0.01)
-COLOR(TANGRAM4, RED)
-R(TANGRAM4, 45)
-M(TANGRAM4, 3, 1)
-TANGRAM5 = TRIANGLE([1, 1], [3, 1], [2, 2])
-TANGRAM5 = PRISM(TANGRAM5, 0.01)
-COLOR(TANGRAM5, CYAN)
-TANGRAM6 = QUAD([0, 0], [2, 0], [1, 1], [3, 1]) 
-TANGRAM6 = PRISM(TANGRAM6, 0.01)
-COLOR(TANGRAM6, PINK)
-TANGRAM7 = TRIANGLE([2, 0], [4, 0], [4, 2])
-TANGRAM7 = PRISM(TANGRAM7, 0.01)
-COLOR(TANGRAM7, ORANGE)
+def TANGRAM1():
+  tangram1 = TRIANGLE([2, 2], [4, 4], [0, 4])
+  tangram1 = PRISM(tangram1, 0.01)
+  COLOR(tangram1, GREEN)
+  return tangram1
+
+def TANGRAM2():
+  tangram2 = TRIANGLE([0, 0], [2, 2], [0, 4])
+  tangram2 = PRISM(tangram2, 0.01)
+  COLOR(tangram2, YELLOW)
+  return tangram2
+
+def TANGRAM3():
+  tangram3 = TRIANGLE([3, 3], [4, 2], [4, 4])
+  tangram3 = PRISM(tangram3, 0.01)
+  COLOR(tangram3, BLUE)
+  return tangram3
+
+def TANGRAM4():
+  tangram4 = SQUARE(1.4142)
+  tangram4 = PRISM(tangram4, 0.01)
+  COLOR(tangram4, RED)
+  R(tangram4, 45)
+  M(tangram4, 3, 1)
+  return tangram4
+
+def TANGRAM5():
+  tangram5 = TRIANGLE([1, 1], [3, 1], [2, 2])
+  tangram5 = PRISM(tangram5, 0.01)
+  COLOR(tangram5, CYAN)
+  return tangram5
+
+def TANGRAM6():
+  tangram6 = QUAD([0, 0], [2, 0], [1, 1], [3, 1]) 
+  tangram6 = PRISM(tangram6, 0.01)
+  COLOR(tangram6, PINK)
+  return tangram6
+
+def TANGRAM7():
+  tangram7 = TRIANGLE([2, 0], [4, 0], [4, 2])
+  tangram7 = PRISM(tangram7, 0.01)
+  COLOR(tangram7, ORANGE)
+  return tangram7
