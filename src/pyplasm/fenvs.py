92794fc0a8e05726ff828eba98c8f0521b41afc9
Pavel Solin <solin.pavel@gmail.com>
2015-03-22 19:42:11 -0700

    Bugfix

--- src/pyplasm/fenvs.py
+++ src/pyplasm/fenvs.py
@@ -3685,7 +3685,7 @@ def SPHERE(radius, divisions = [16, 32]):
             raise ExceptionWT("Bad division in the SPHERE command!")
         divisionslist = [divisions/2, divisions]
     # Returning the sphere:
-    return SHELL(0, radius, divisionslist)
+    return BASEOBJ(JOIN(PLASM_SPHERE(radius)(divisions)))
 
 # Czech:
 KOULE = SPHERE
